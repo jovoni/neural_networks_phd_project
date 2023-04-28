a2452f97b8a9997b45cb6dfa56aabb899e78b954
Giovanni Santacatterina <jovoni@Air-di-Giovanni.lan>
2023-04-28 19:53:06 +0200

    pretrained net + deep net

--- train.py
+++ train.py
@@ -4,15 +4,16 @@ import torch.nn as nn
 import torch.optim as optim
 from data import get_dataloaders
 import pandas as pd
-from twoLNN import twoLNN
+from twoLNN import twoLNN, fiveLNN
 from utils import use_gpu_if_possible
 import argparse
 
 MODELS = {
-    "twoLNN": twoLNN
+    "twoLNN": twoLNN,
+    "fiveLNN": fiveLNN
 }
 
-def train(model_name, task_name, K, batch_size=128, nepochs=20, lr=.01):
+def train_parity(model_name, K, batch_size=128, nepochs=20, lr=.01):
     # get device
     device = use_gpu_if_possible()
 
@@ -31,7 +32,7 @@ def train(model_name, task_name, K, batch_size=128, nepochs=20, lr=.01):
         print(f"epoch: {e+1}")
 
         # Prep data
-        dataloaders = get_dataloaders(K, batch_size)
+        dataloaders = get_dataloaders("parity", K, batch_size)
 
         running_loss = 0.0
         train_acc = 0
@@ -71,7 +72,85 @@ def train(model_name, task_name, K, batch_size=128, nepochs=20, lr=.01):
         print(f"\tTest acc  = {test_acc:.4f}")
         
     # save results
-    name = f'{task_name}_{model_name}_{K}_{lr}_{batch_size}_{nepochs}'
+    name = f'parity_{model_name}_{K}_{lr}_{batch_size}_{nepochs}'
+
+    results_path = "results/" + name + ".csv"
+    results.to_csv(results_path, sep=",")
+
+    # save model
+    model_path = "models/" + name + ".pt"
+    torch.save(model.state_dict(), model_path)
+    
+    print("DONE") 
+
+
+def train_classification(model_name, K, batch_size=128, nepochs=20, lr=.01):
+    # get device
+    device = use_gpu_if_possible()
+    device = "cpu"
+
+    print(f"Using device : {device}")
+
+    # Load model freezing certain parameters
+    trained_model_path = f'models/parity_{model_name}_{K}_{lr}_{batch_size}_{nepochs}.pt'
+    model = MODELS[model_name](K).to(device)
+    model.load_state_dict(torch.load(trained_model_path))
+    for name, param in model.named_parameters():
+        if name.startswith("first"):
+            param.requires_grad=False
+    
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    # Init results table
+    results = pd.DataFrame()
+
+    # Start training
+    for e in range(nepochs):
+        print(f"epoch: {e+1}")
+
+        # Prep data
+        dataloaders = get_dataloaders("classification", K, batch_size)
+
+        running_loss = 0.0
+        train_acc = 0
+        for _, batch in enumerate(dataloaders['train']):
+            X, Y = batch            
+            X = X.to(device)
+            Y = Y.to(device).long()
+            out = model.classify(X)
+
+            loss = loss_function(out, Y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            
+            out = torch.argmax(out, dim=1).reshape(-1)
+            train_acc += (out.round() == Y).sum()
+
+        # Test Evalutaion
+        test_acc = 0
+        for X, Y in dataloaders['test']:
+            X = X.to(device)
+            Y = Y.to(device).long()
+
+            out = torch.argmax(model.classify(X), dim=1).reshape(-1)
+            test_acc += (out.round() == Y).sum()
+
+        train_acc = train_acc / len(dataloaders['train'].dataset)
+        test_acc = test_acc / len(dataloaders['test'].dataset)
+
+        new_results = pd.DataFrame({"epoch": e, "train_acc": train_acc.item(), "test_acc": test_acc.item()}, index=[0])
+        results = pd.concat([results, new_results])
+
+        print(f"\tTrain acc = {train_acc:.4f}")
+        print(f"\tTest acc  = {test_acc:.4f}")
+        
+    # save results
+    name = f'classification_{model_name}_{K}_{lr}_{batch_size}_{nepochs}'
 
     results_path = "results/" + name + ".csv"
     results.to_csv(results_path, sep=",")
@@ -97,4 +176,7 @@ if __name__ == "__main__":
     task_name = args.task_name
     model_name = args.model_name
 
-    train(model_name=model_name, task_name=task_name, K=k, lr=lr)
+    if task_name == "parity":
+        train_parity(model_name=model_name, K=k, lr=lr)
+    else:
+        train_classification(model_name=model_name, K=k, lr=lr)
--- twoLNN.py
+++ twoLNN.py
@@ -28,5 +28,38 @@ class twoLNN(nn.Module):
         x = self.first_section(x)
         x = self.classification(x)
         return(x)
+    
 
+class fiveLNN(nn.Module):
 
+    def __init__(self, K):
+        # K inidicating the number of MNIST images
+
+        super(fiveLNN, self).__init__()
+        self.K = K
+
+        self.first_section = nn.Sequential(
+            nn.Linear(28 * 28 * K, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU()
+        )
+
+        self.parity = nn.Linear(64, 1)
+        self.classification = nn.Linear(64,10)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28*self.K)
+        x = self.first_section(x)
+        x = self.parity(x)
+        return(x)
+    
+    def classify(self, x):
+        x = x.view(-1, 28*28*self.K)
+        x = self.first_section(x)
+        x = self.classification(x)
+        return(x)
