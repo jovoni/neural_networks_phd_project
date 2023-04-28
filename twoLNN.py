66c4dc5ff46730f3ac876c673b2a2a1146a273ea
valerianilucrezia <lucreziavaleriani@Air-di-Lucrezia.lan>
2023-04-28 18:56:46 +0200

    add classification

--- twoLNN.py
+++ twoLNN.py
@@ -7,15 +7,26 @@ class twoLNN(nn.Module):
         # K inidicating the number of MNIST images
 
         super(twoLNN, self).__init__()
-
         self.K = K
-        self.layer1 = nn.Linear(28 * 28 * K, 512)
-        self.activation = nn.ReLU()
-        self.layer2 = nn.Linear(512, 1)
+
+        self.first_section = nn.Sequential(
+            nn.Linear(28 * 28 * K, 512),
+            nn.ReLU()
+        )
+
+        self.parity = nn.Linear(512, 1)
+        self.classification = nn.Linear(512,10)
 
     def forward(self, x):
         x = x.view(-1, 28*28*self.K)
-        x = self.layer1(x)
-        x = self.activation(x)
-        x = self.layer2(x)
+        x = self.first_section(x)
+        x = self.parity(x)
         return(x)
+    
+    def classify(self, x):
+        x = x.view(-1, 28*28*self.K)
+        x = self.first_section(x)
+        x = self.classification(x)
+        return(x)
+
+
