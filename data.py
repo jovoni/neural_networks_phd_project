12c0baa364b66d6b6dda5f017f41b783cbade26b
Giovanni Santacatterina <jovoni@Air-di-Giovanni.lan>
2023-04-28 18:57:21 +0200

    change dataloader for classifcation

--- data.py
+++ data.py
@@ -6,13 +6,16 @@ from torch.utils.data import DataLoader, Dataset
 
 class MnistKDataset(Dataset):
 
-    def __init__(self, data, K):
+    def __init__(self, data, K, task):
         indices = torch.empty(K, len(data), dtype=torch.int64)
         for k in range(K):
             indices[k] = torch.randperm(len(data))
 
         self.data = torch.cat([data.data[indices[i]] for i in range(K)], dim=2)
-        self.targets = sum([data.targets[indices[i]] for i in range(K)]) % 2
+        if task == "parity":
+            self.targets = sum([data.targets[indices[i]] for i in range(K)]) % 2
+        elif task == "classification":
+            self.targets = data.targets[indices[0]]
 
     def __len__(self):
         return len(self.data)
@@ -22,15 +25,15 @@ class MnistKDataset(Dataset):
         y = self.targets[idx].float()
         return X, y
     
-def get_dataloaders(K, batch_size):
+def get_dataloaders(task, K, batch_size):
     data_sets = {
         "train": datasets.MNIST(root="datasets/mnist", train=True, download=True),
         "test": datasets.MNIST(root="datasets/mnist", train=False, download=True)
         }
     
     dataloaders = {
-        "train" : DataLoader(dataset=MnistKDataset(data=data_sets['train'], K=K), batch_size=batch_size), 
-        "test"  : DataLoader(dataset=MnistKDataset(data=data_sets['test'], K=K), batch_size=batch_size)
+        "train" : DataLoader(dataset=MnistKDataset(data=data_sets['train'], K=K, task=task), batch_size=batch_size), 
+        "test"  : DataLoader(dataset=MnistKDataset(data=data_sets['test'], K=K, task=task), batch_size=batch_size)
     }
 
     return dataloaders
