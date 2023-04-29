de903acb424063e1ff3789b676df9448ad901731
valerianilucrezia <lucreziavaleriani@Air-di-Lucrezia.lan>
2023-04-29 13:37:00 +0200

    add LeNet

--- models.py
+++ models.py
@@ -75,5 +75,34 @@ class fiveLNN(nn.Module):
         return(x)
     
 class LeNet(nn.Module):
-    # to implement from scratch
-    pass
+    def __init__(self, K):
+        super(LeNet, self).__init__()
+
+        self.K = K
+        
+        self.first_section = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, padding=0),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Flatten(),
+            nn.Linear(16*5*(5+7*K-7), 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU()
+        )
+
+        self.parity = nn.Linear(84,1)
+        self.classification = nn.Linear(84,10)
+
+    def forward(self,x):        
+        x = self.first_section(x)
+        x = self.parity(x)
+        return(x)
+    
+    def classify(self, x):
+        x = self.first_section(x)
+        x = self.classification(x)
+        return(x)
