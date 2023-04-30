d98cc14ae338b15b6ba8bf629e59a07659ecccc9
jovoni <giovy.santa@gmail.com>
2023-04-30 17:25:11 +0200

    mc

--- cca.py
+++ cca.py
@@ -34,7 +34,7 @@ def extract_representation(model_name, task, K, save=False):
     if save:
         np.save(f"representations/{task}_{model_name}_{K}.npy", all_representations.numpy())
 
-def svcca(model_names, task_names, K):
+def svcca(model_names, task_names, K, explained_variance = .99):
 
     Xs = []
     for model_name, task in zip(model_names, task_names):
@@ -43,13 +43,20 @@ def svcca(model_names, task_names, K):
         file_name = f"representations/{task}_{model_name}_{K}.npy"
 
         X = np.load(file_name)
-        Xs.append(pca(X))
+        Xs.append(pca(X, explained_variance=explained_variance))
 
     return cca(Xs[0], Xs[1])
 
 
 def cca(X1, X2):
+    print(X1.shape)
+    print(X2.shape)
+
     n_comps = min(X1.shape[1], X2.shape[1])
+
+    print(n_comps)
+
+    
     cca = CCA(n_components=n_comps)
     cca.fit(X1, X2)
     return cca.correlation_
@@ -79,7 +86,7 @@ if __name__ == "__main__":
     # Same model different task
     for m in MODELS.keys():
         for k in [1,3]:
-            result = svcca([m,m], ['parity', 'classification'], k)
+            result = svcca([m,m], ['parity', 'classification'], k, explained_variance=.9)
             print( m, k, result)
 
     # Same task different models
